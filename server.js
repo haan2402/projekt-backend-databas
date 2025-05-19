70ca2d9630077b0256772b2f659fb0fa5b866174
Hanna Angeria <haan2402@student.miun.se>
2025-05-19 14:40:53 +0200

    gjort grund till min server.js och döpt om till menu.js för enklare struktur

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/authRoute");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+//ansluter till MongoDB databas, Atlas
+mongoose.connect(process.env.DATABASE).then(() => {
+    console.log("Ansluten till MongoDB databas");
+}). catch((error) => {
+    console.log("Gick inte att ansluta till databas.." + error);
+});
+
+//startar applikationen
+app.listen(port, () => {
+    console.log(`Server is running on: http://localhost:${port}`);
+});
